a87f8997c55308704aa1c30ce641fad9fe19b795
rabby-web <rabby.webdeveloper@gmail.com>
2024-08-10 11:28:02 +0600

    cart data get

--- index.js
+++ index.js
@@ -41,6 +41,11 @@ async function run() {
       res.send(result);
     });
     // cart collection
+    app.get("/carts", async (req, res) => {
+      const result = await cartCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post("/carts", async (req, res) => {
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
