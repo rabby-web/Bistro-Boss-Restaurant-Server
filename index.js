ff4e9d25c91c8d6e20d9bb032523be0bd1225cd6
rabby-web <rabby.webdeveloper@gmail.com>
2024-08-12 12:02:14 +0600

    delete users

--- index.js
+++ index.js
@@ -38,6 +38,13 @@ async function run() {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
+    // delete user
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // post user
     app.post("/users", async (req, res) => {
