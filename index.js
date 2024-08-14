dd0efa7ff6e3ce8bee3a085ff174c53590630784
rabby-web <rabby.webdeveloper@gmail.com>
2024-08-14 09:36:12 +0600

    check admin

--- index.js
+++ index.js
@@ -59,10 +59,25 @@ async function run() {
     };
 
     // user related api
+
     // get users
     app.get("/users", verifyToken, async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
+    });
+
+    app.get("/users/admin/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "unauthorized access" });
+      }
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
+      }
+      res.send({ admin });
     });
     // delete user
     app.delete("/users/:id", async (req, res) => {
